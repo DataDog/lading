afa6a722db1c2983e1598ce210023045718f8131
Brian L. Troutwine <brian@troutwine.us>
2022-03-15 14:07:25 -0700

    + splunk_hec
    Signed-off-by: Brian L. Troutwine <brian@troutwine.us>

--- lading_rig/src/generator.rs
+++ lading_rig/src/generator.rs
@@ -2,11 +2,13 @@ use crate::signals::Shutdown;
 use serde::Deserialize;
 
 pub mod http;
+pub mod splunk_hec;
 pub mod tcp;
 
 pub enum Error {
     Tcp(tcp::Error),
     Http(http::Error),
+    SplunkHec(splunk_hec::Error),
 }
 
 #[derive(Debug, Deserialize)]
@@ -14,11 +16,13 @@ pub enum Error {
 pub enum Config {
     Tcp(tcp::Config),
     Http(http::Config),
+    SplunkHec(splunk_hec::Config),
 }
 
 pub enum Server {
     Tcp(tcp::Tcp),
     Http(http::Http),
+    SplunkHec(splunk_hec::SplunkHec),
 }
 
 impl Server {
@@ -26,6 +30,9 @@ impl Server {
         match config {
             Config::Tcp(conf) => Self::Tcp(tcp::Tcp::new(conf, shutdown).unwrap()),
             Config::Http(conf) => Self::Http(http::Http::new(conf, shutdown).unwrap()),
+            Config::SplunkHec(conf) => {
+                Self::SplunkHec(splunk_hec::SplunkHec::new(conf, shutdown).unwrap())
+            }
         }
     }
 
@@ -33,6 +40,7 @@ impl Server {
         match self {
             Server::Tcp(inner) => inner.spin().await.map_err(Error::Tcp),
             Server::Http(inner) => inner.spin().await.map_err(Error::Http),
+            Server::SplunkHec(inner) => inner.spin().await.map_err(Error::SplunkHec),
         }
     }
 }
