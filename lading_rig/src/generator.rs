883c3ddb00bbd7e9023cd144f2ffef66905bc6cf
Brian L. Troutwine <brian@troutwine.us>
2022-03-15 11:59:10 -0700

    + http generator
    Signed-off-by: Brian L. Troutwine <brian@troutwine.us>

--- lading_rig/src/generator.rs
+++ lading_rig/src/generator.rs
@@ -1,32 +1,38 @@
 use crate::signals::Shutdown;
 use serde::Deserialize;
 
+pub mod http;
 pub mod tcp;
 
 pub enum Error {
     Tcp(tcp::Error),
+    Http(http::Error),
 }
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "snake_case")]
 pub enum Config {
     Tcp(tcp::Config),
+    Http(http::Config),
 }
 
 pub enum Server {
     Tcp(tcp::Tcp),
+    Http(http::Http),
 }
 
 impl Server {
     pub fn new(config: Config, shutdown: Shutdown) -> Self {
         match config {
             Config::Tcp(conf) => Self::Tcp(tcp::Tcp::new(conf, shutdown).unwrap()),
+            Config::Http(conf) => Self::Http(http::Http::new(conf, shutdown).unwrap()),
         }
     }
 
     pub async fn run(self) -> Result<(), Error> {
         match self {
             Server::Tcp(inner) => inner.spin().await.map_err(Error::Tcp),
+            Server::Http(inner) => inner.spin().await.map_err(Error::Http),
         }
     }
 }
---
+++ lading_rig/src/generator/http.rs
@@ -0,0 +1,322 @@
+use byte_unit::{Byte, ByteUnit};
+use futures::stream::{self, StreamExt};
+use governor::state::direct::{self, InsufficientCapacity};
+use governor::{clock, state, Quota, RateLimiter};
+use hyper::client::Client;
+use hyper::client::HttpConnector;
+use hyper::header::CONTENT_LENGTH;
+use hyper::{Body, HeaderMap, Request, Uri};
+use lading_common::block::{self, chunk_bytes, construct_block_cache, Block};
+use lading_common::payload;
+use metrics::{counter, gauge};
+use once_cell::sync::OnceCell;
+use rand::prelude::StdRng;
+use rand::SeedableRng;
+use serde::Deserialize;
+use std::num::NonZeroU32;
+use std::path::PathBuf;
+use std::sync::Arc;
+use tokio::sync::Semaphore;
+use tracing::info;
+
+use crate::signals::Shutdown;
+
+static CONNECTION_SEMAPHORE: OnceCell<Semaphore> = OnceCell::new();
+
+/// The HTTP method to be used in requests
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum Method {
+    /// Make HTTP Post requests
+    Post {
+        /// The payload generator to use for this target
+        variant: Variant,
+        /// The maximum size in bytes of the cache of prebuilt messages
+        maximum_prebuild_cache_size_bytes: byte_unit::Byte,
+    },
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum Variant {
+    /// Generates Splunk HEC messages
+    SplunkHec,
+    /// Generates Datadog Logs JSON messages
+    DatadogLog,
+    /// Generates a limited subset of FoundationDB logs
+    FoundationDb,
+    /// Generates a static, user supplied data
+    Static {
+        /// Defines the file path to read static variant data from. Content is
+        /// assumed to be line-oriented but no other claim is made on the file.
+        static_path: PathBuf,
+    },
+    /// Generates a line of printable ascii characters
+    Ascii,
+    /// Generates a json encoded line
+    Json,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    /// The seed for random operations against this target
+    pub seed: [u8; 32],
+    /// The URI for the target, must be a valid URI
+    #[serde(with = "http_serde::uri")]
+    pub target_uri: Uri,
+    /// The method to use against the URI
+    pub method: Method,
+    /// Headers to include in the request
+    #[serde(with = "http_serde::header_map")]
+    pub headers: HeaderMap,
+    /// The bytes per second to send or receive from the target
+    pub bytes_per_second: byte_unit::Byte,
+    /// The block sizes for messages to this target
+    pub block_sizes: Option<Vec<byte_unit::Byte>>,
+    /// The total number of parallel connections to maintain
+    pub parallel_connections: u16,
+}
+
+#[derive(Debug)]
+pub enum Error {
+    Governor(InsufficientCapacity),
+    Io(::std::io::Error),
+    Block(block::Error),
+    Hyper(hyper::Error),
+    Http(hyper::http::Error),
+}
+
+impl From<block::Error> for Error {
+    fn from(error: block::Error) -> Self {
+        Error::Block(error)
+    }
+}
+
+impl From<hyper::Error> for Error {
+    fn from(error: hyper::Error) -> Self {
+        Error::Hyper(error)
+    }
+}
+
+impl From<hyper::http::Error> for Error {
+    fn from(error: hyper::http::Error) -> Self {
+        Error::Http(error)
+    }
+}
+
+impl From<InsufficientCapacity> for Error {
+    fn from(error: InsufficientCapacity) -> Self {
+        Error::Governor(error)
+    }
+}
+
+impl From<::std::io::Error> for Error {
+    fn from(error: ::std::io::Error) -> Self {
+        Error::Io(error)
+    }
+}
+
+/// The [`Worker`] defines a task that emits variant lines to an HTTP server
+/// controlling throughput.
+#[derive(Debug)]
+pub struct Http {
+    uri: Uri,
+    method: hyper::Method,
+    headers: hyper::HeaderMap,
+    parallel_connections: u16,
+    rate_limiter: RateLimiter<direct::NotKeyed, state::InMemoryState, clock::QuantaClock>,
+    block_cache: Vec<Block>,
+    metric_labels: Vec<(String, String)>,
+    shutdown: Shutdown,
+}
+
+impl Http {
+    /// Create a new [`Worker`] instance
+    ///
+    /// # Errors
+    ///
+    /// Creation will fail if the underlying governor capacity exceeds u32.
+    ///
+    /// # Panics
+    ///
+    /// Function will panic if user has passed non-zero values for any byte
+    /// values. Sharp corners.
+    #[allow(clippy::cast_possible_truncation)]
+    pub fn new(config: Config, shutdown: Shutdown) -> Result<Self, Error> {
+        let mut rng = StdRng::from_seed(config.seed);
+        let block_sizes: Vec<usize> = config
+            .block_sizes
+            .unwrap_or_else(|| {
+                vec![
+                    Byte::from_unit(1.0 / 8.0, ByteUnit::MB).unwrap(),
+                    Byte::from_unit(1.0 / 4.0, ByteUnit::MB).unwrap(),
+                    Byte::from_unit(1.0 / 2.0, ByteUnit::MB).unwrap(),
+                    Byte::from_unit(1_f64, ByteUnit::MB).unwrap(),
+                    Byte::from_unit(2_f64, ByteUnit::MB).unwrap(),
+                    Byte::from_unit(4_f64, ByteUnit::MB).unwrap(),
+                ]
+            })
+            .iter()
+            .map(|sz| sz.get_bytes() as usize)
+            .collect();
+        let bytes_per_second = NonZeroU32::new(config.bytes_per_second.get_bytes() as u32).unwrap();
+        let rate_limiter = RateLimiter::direct(Quota::per_second(bytes_per_second));
+        let labels = vec![];
+        match config.method {
+            Method::Post {
+                variant,
+                maximum_prebuild_cache_size_bytes,
+            } => {
+                let block_chunks = chunk_bytes(
+                    &mut rng,
+                    maximum_prebuild_cache_size_bytes.get_bytes() as usize,
+                    &block_sizes,
+                );
+                let block_cache = match variant {
+                    Variant::Ascii => construct_block_cache(
+                        &mut rng,
+                        &payload::Ascii::default(),
+                        &block_chunks,
+                        &labels,
+                    ),
+                    Variant::SplunkHec => construct_block_cache(
+                        &mut rng,
+                        &payload::SplunkHec::default(),
+                        &block_chunks,
+                        &labels,
+                    ),
+                    Variant::DatadogLog => construct_block_cache(
+                        &mut rng,
+                        &payload::DatadogLog::default(),
+                        &block_chunks,
+                        &labels,
+                    ),
+                    Variant::Json => construct_block_cache(
+                        &mut rng,
+                        &payload::Json::default(),
+                        &block_chunks,
+                        &labels,
+                    ),
+                    Variant::FoundationDb => construct_block_cache(
+                        &mut rng,
+                        &payload::FoundationDb::default(),
+                        &block_chunks,
+                        &labels,
+                    ),
+                    Variant::Static { static_path } => construct_block_cache(
+                        &mut rng,
+                        &payload::Static::new(&static_path),
+                        &block_chunks,
+                        &labels,
+                    ),
+                };
+
+                CONNECTION_SEMAPHORE
+                    .set(Semaphore::new(config.parallel_connections as usize))
+                    .unwrap();
+
+                Ok(Self {
+                    parallel_connections: config.parallel_connections,
+                    uri: config.target_uri,
+                    method: hyper::Method::POST,
+                    headers: config.headers,
+                    block_cache,
+                    rate_limiter,
+                    metric_labels: labels,
+                    shutdown,
+                })
+            }
+        }
+    }
+
+    /// Enter the main loop of this [`Worker`]
+    ///
+    /// # Errors
+    ///
+    /// TODO
+    ///
+    /// # Panics
+    ///
+    /// Function will panic if it is unable to create HTTP requests for the
+    /// target.
+    pub async fn spin(mut self) -> Result<(), Error> {
+        let client: Client<HttpConnector, Body> = Client::builder()
+            .pool_max_idle_per_host(self.parallel_connections as usize)
+            .retry_canceled_requests(false)
+            .set_host(false)
+            .build_http();
+        let rate_limiter = Arc::new(self.rate_limiter);
+        let method = self.method;
+        let uri = self.uri;
+
+        let labels = self.metric_labels;
+        gauge!(
+            "maximum_requests",
+            f64::from(self.parallel_connections),
+            &labels
+        );
+
+        let mut block_stream = stream::iter(self.block_cache.iter().cycle());
+
+        loop {
+            tokio::select! {
+                blk = block_stream.next() => {
+                    let blk = blk.unwrap();
+
+                    let client = client.clone();
+                    let rate_limiter = Arc::clone(&rate_limiter);
+                    let labels = labels.clone();
+                    let method = method.clone();
+                    let uri = uri.clone();
+
+                    let total_bytes = blk.total_bytes;
+                    let body = Body::from(blk.bytes.clone());
+                    let block_length = blk.bytes.len();
+
+                    let mut request: Request<Body> = Request::builder()
+                        .method(method)
+                        .uri(uri)
+                        .header(CONTENT_LENGTH, block_length)
+                        .body(body)
+                        .unwrap();
+                    let headers = request.headers_mut();
+                    for (k,v) in self.headers.clone().drain() {
+                        if let Some(k) = k {
+                            headers.insert(k,v);
+                        }
+                    }
+
+                    let permit = CONNECTION_SEMAPHORE.get().unwrap().acquire().await.unwrap();
+                    tokio::spawn(async move {
+                        rate_limiter.until_n_ready(total_bytes).await.unwrap();
+                        counter!("requests_sent", 1, &labels);
+                        match client.request(request).await {
+                            Ok(response) => {
+                                counter!("bytes_written", block_length as u64, &labels);
+                                let status = response.status();
+                                let mut status_labels = labels.clone();
+                                status_labels
+                                    .push(("status_code".to_string(), status.as_u16().to_string()));
+                                counter!("request_ok", 1, &status_labels);
+                            }
+                            Err(err) => {
+                                let mut error_labels = labels.clone();
+                                error_labels.push(("error".to_string(), err.to_string()));
+                                error_labels.push(("body_size".to_string(), block_length.to_string()));
+                                counter!("request_failure", 1, &error_labels);
+                            }
+                        }
+                        drop(permit);
+                    });
+                },
+                _ = self.shutdown.recv() => {
+                    info!("shutdown signal received");
+                    // Acquire all available connections, meaning that we have
+                    // no outstanding tasks in flight.
+                    let _ = CONNECTION_SEMAPHORE.get().unwrap().acquire_many(self.parallel_connections as u32).await.unwrap();
+                    return Ok(());
+                },
+            }
+        }
+    }
+}
