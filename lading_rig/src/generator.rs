3eb2b85b8033550d1fd6a0216c9d66b0a858e7fa
Brian L. Troutwine <brian@troutwine.us>
2022-03-15 16:35:46 -0700

    now + file_gen
    Signed-off-by: Brian L. Troutwine <brian@troutwine.us>

--- lading_rig/src/generator.rs
+++ lading_rig/src/generator.rs
@@ -1,7 +1,9 @@
 use crate::signals::Shutdown;
 use serde::Deserialize;
 
+pub mod file_gen;
 pub mod http;
+pub mod kafka;
 pub mod splunk_hec;
 pub mod tcp;
 
@@ -9,6 +11,8 @@ pub enum Error {
     Tcp(tcp::Error),
     Http(http::Error),
     SplunkHec(splunk_hec::Error),
+    Kafka(kafka::Error),
+    FileGen(file_gen::Error),
 }
 
 #[derive(Debug, Deserialize)]
@@ -17,12 +21,16 @@ pub enum Config {
     Tcp(tcp::Config),
     Http(http::Config),
     SplunkHec(splunk_hec::Config),
+    Kafka(kafka::Config),
+    FileGen(file_gen::Config),
 }
 
 pub enum Server {
     Tcp(tcp::Tcp),
     Http(http::Http),
     SplunkHec(splunk_hec::SplunkHec),
+    Kafka(kafka::Kafka),
+    FileGen(file_gen::FileGen),
 }
 
 impl Server {
@@ -33,6 +41,8 @@ impl Server {
             Config::SplunkHec(conf) => {
                 Self::SplunkHec(splunk_hec::SplunkHec::new(conf, shutdown).unwrap())
             }
+            Config::Kafka(conf) => Self::Kafka(kafka::Kafka::new(conf, shutdown).unwrap()),
+            Config::FileGen(conf) => Self::FileGen(file_gen::FileGen::new(conf, shutdown).unwrap()),
         }
     }
 
@@ -41,6 +51,8 @@ impl Server {
             Server::Tcp(inner) => inner.spin().await.map_err(Error::Tcp),
             Server::Http(inner) => inner.spin().await.map_err(Error::Http),
             Server::SplunkHec(inner) => inner.spin().await.map_err(Error::SplunkHec),
+            Server::Kafka(inner) => inner.spin().await.map_err(Error::Kafka),
+            Server::FileGen(inner) => inner.spin().await.map_err(Error::FileGen),
         }
     }
 }
