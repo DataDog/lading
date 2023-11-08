f3a4d919697fab249ef985770727e4304a260ee5
Brian L. Troutwine <brian.troutwine@datadoghq.com>
2023-11-08 12:12:40 -0800

    Convert target metrics where possible to stream
    This commit converts our expvar and prometheus metrics to the use of streaming
    bodies where possible, especially on the prometheus side. The implementation of
    both is adjusted to be similar and we now record the content-length from the
    target.
    
    REF SMPTNG-79
    
    Signed-off-by: Brian L. Troutwine <brian.troutwine@datadoghq.com>

--- lading/src/target_metrics/expvar.rs
+++ lading/src/target_metrics/expvar.rs
@@ -5,7 +5,7 @@
 
 use std::time::Duration;
 
-use metrics::gauge;
+use metrics::{gauge, register_histogram};
 use serde::Deserialize;
 use serde_json::Value;
 use tracing::{error, info, trace};
@@ -62,39 +62,42 @@ impl Expvar {
         info!("Expvar target metrics scraper running");
         let client = reqwest::Client::new();
 
-        let server = async move {
-            loop {
-                tokio::time::sleep(Duration::from_secs(1)).await;
+        let mut timer = tokio::time::interval(Duration::from_secs(1));
 
-                let Ok(resp) = client.get(&self.config.uri).timeout(Duration::from_secs(1)).send().await else {
-                    info!("failed to get expvar uri");
-                    continue;
-                };
+        let content_length =
+            register_histogram!("target_metrics_content_length", "source" => "expvar");
 
-                let Ok(json) = resp.json::<Value>().await else {
-                    info!("failed to parse expvar json");
-                    continue;
-                };
+        loop {
+            tokio::select! {
+                _ = timer.tick() => {
+                    let Ok(resp) = client.get(&self.config.uri).timeout(Duration::from_secs(1)).send().await else {
+                        info!("failed to get expvar uri");
+                        continue;
+                    };
+                    if let Some(cl) = resp.content_length() {
+                        content_length.record(cl as f64);
+                    }
 
-                for var_name in &self.config.vars {
-                    let val = json.pointer(var_name).and_then(serde_json::Value::as_f64);
-                    if let Some(val) = val {
-                        trace!("expvar: {} = {}", var_name, val);
-                        gauge!(format!("target/{name}", name = var_name.trim_start_matches('/')), val, "source" => "target_metrics/expvar");
+                    let Ok(json) = resp.json::<Value>().await else {
+                        info!("failed to parse expvar json");
+                        continue;
+                    };
+
+                    for var_name in &self.config.vars {
+                        let val = json.pointer(var_name).and_then(serde_json::Value::as_f64);
+                        if let Some(val) = val {
+                            trace!("expvar: {} = {}", var_name, val);
+                            gauge!(format!("target/{name}", name = var_name.trim_start_matches('/')), val, "source" => "target_metrics/expvar");
+                        }
                     }
                 }
-            }
-        };
-
-        tokio::select! {
-            _res = server => {
-                error!("server shutdown unexpectedly");
-                 Err(Error::EarlyShutdown)
-            }
-            _ = self.shutdown.recv() => {
-                info!("shutdown signal received");
-                 Ok(())
+                _ = self.shutdown.recv() => {
+                    info!("shutdown signal received");
+                    break;
+                }
             }
         }
+
+        Ok(())
     }
 }
