2814aec5075164578ed73cae44385526e4a04811
Brian L. Troutwine <brian.troutwine@datadoghq.com>
2023-11-09 18:25:23 -0800

    Introduce fuzzing for `lading_payload::block::Cache::fixed`
    This commit introduces a new fuzzing harness for the above mentioned
    function. We want to ensure that we are not crashing at runtime or consuming
    more memory than allowed.
    
    REF SMPTNG-79
    
    Signed-off-by: Brian L. Troutwine <brian.troutwine@datadoghq.com>

---
+++ lading_payload/fuzz/fuzz_targets/fixed_cache_creation.rs
@@ -0,0 +1,33 @@
+#![no_main]
+
+use arbitrary;
+use libfuzzer_sys::fuzz_target;
+use rand::{rngs::StdRng, SeedableRng};
+use std::num::NonZeroU32;
+
+use lading_payload::block::Cache;
+
+#[derive(arbitrary::Arbitrary, Debug)]
+struct Input {
+    seed: [u8; 32],
+    total_bytes: NonZeroU32,
+    block_bytes_sizes: [NonZeroU32; 32],
+    payload: lading_payload::Config,
+}
+
+fuzz_target!(|input: Input| {
+    for byte_size in &input.block_bytes_sizes {
+        if byte_size > &input.total_bytes {
+            return;
+        }
+    }
+
+    let mut rng = StdRng::from_seed(input.seed);
+    let res = Cache::fixed(
+        &mut rng,
+        input.total_bytes,
+        &input.block_bytes_sizes,
+        &input.payload,
+    );
+    res.unwrap();
+});
