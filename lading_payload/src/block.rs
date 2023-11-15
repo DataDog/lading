DataDog/lading

commit 72f41d64552f6fde7b41cbc42cca097bbb00d605

Introduce fuzzing for `lading_payload::block::Cache::fixed` (#727)
Introduce fuzzing for `lading_payload::block::Cache::fixed`

This commit introduces a new fuzzing harness for the above mentioned
function. We want to ensure that we are not crashing at runtime or consuming
more memory than allowed.

REF SMPTNG-79

Signed-off-by: Brian L. Troutwine <brian.troutwine@datadoghq.com>

lading_payload/src/block.rs
@@ -12,6 +12,8 @@ use rand::{prelude::SliceRandom, rngs::StdRng, Rng, SeedableRng};
 use serde::Deserialize;
 use tokio::sync::mpsc::{self, error::SendError, Sender};
 
+use crate::dogstatsd;
+
 const MAX_CHUNKS: usize = 4096;
 
 /// Error for `Cache::spin`
@@ -20,6 +22,12 @@ pub enum SpinError {
     /// See [`SendError`]
     #[error(transparent)]
     Send(#[from] SendError<Block>),
+    /// Provided configuration had validation errors
+    #[error("Provided configuration was not valid.")]
+    InvalidConfig,
+    /// DogStatsD creation error
+    #[error(transparent)]
+    DogStatsD(#[from] dogstatsd::Error),
 }
 
 /// Error for [`Cache`]
@@ -28,6 +36,15 @@ pub enum Error {
     /// See [`ChunkError`]
     #[error("Chunk error: {0}")]
     Chunk(#[from] ChunkError),
+    /// See [`ConstructBlockCacheError`]
+    #[error(transparent)]
+    Construct(#[from] ConstructBlockCacheError),
+    /// Provided configuration had validation errors
+    #[error("Provided configuration was not valid.")]
+    InvalidConfig,
+    /// DogStatsD creation error
+    #[error(transparent)]
+    DogStatsD(#[from] dogstatsd::Error),
 }
 
 /// Errors for the construction of chunks
@@ -50,6 +67,26 @@ pub struct Block {
     pub bytes: Bytes,
 }
 
+/// Errors for the construction of the block cache
+#[derive(Debug, thiserror::Error, Clone, Copy)]
+pub enum ConstructBlockCacheError {
+    /// All blocks sizes were insufficient
+    #[error("Insufficient block sizes.")]
+    InsufficientBlockSizes,
+}
+
+#[cfg(feature = "arbitrary")]
+impl<'a> arbitrary::Arbitrary<'a> for Block {
+    fn arbitrary(u: &mut arbitrary::Unstructured<'a>) -> arbitrary::Result<Self> {
+        let total_bytes = u32::arbitrary(u)?;
+        let bytes = u.bytes(total_bytes as usize).map(Bytes::copy_from_slice)?;
+        Ok(Self {
+            total_bytes: NonZeroU32::new(total_bytes).unwrap(),
+            bytes,
+        })
+    }
+}
+
 #[derive(Debug, Deserialize, PartialEq, Clone, Copy)]
 /// The method for which caching will be configure
 pub enum CacheMethod {
@@ -66,6 +103,7 @@ pub fn default_cache_method() -> CacheMethod {
 }
 
 #[derive(Debug)]
+#[cfg_attr(feature = "arbitrary", derive(arbitrary::Arbitrary))]
 /// A mechanism for streaming byte blobs, 'blocks'
 ///
 /// The `Cache` is a mechanism to allow generators to request 'blocks' without
@@ -155,26 +193,33 @@ impl Cache {
                     crate::Encoding::MsgPack => crate::TraceAgent::msg_pack(&mut rng),
                 };
 
-                construct_block_cache_inner(&mut rng, &ta, &block_chunks)
+                construct_block_cache_inner(&mut rng, &ta, &block_chunks)?
             }
             crate::Config::Syslog5424 => {
-                construct_block_cache_inner(&mut rng, &crate::Syslog5424::default(), &block_chunks)
+                construct_block_cache_inner(&mut rng, &crate::Syslog5424::default(), &block_chunks)?
             }
-            crate::Config::DogStatsD(crate::dogstatsd::Config {
-                contexts,
-                name_length,
-                tag_key_length,
-                tag_value_length,
-                tags_per_msg,
-                // TODO -- Validate user input for multivalue_pack_probability.
-                multivalue_pack_probability,
-                multivalue_count,
-                kind_weights,
-                metric_weights,
-                value,
-            }) => {
+            crate::Config::DogStatsD(
+                conf @ crate::dogstatsd::Config {
+                    contexts,
+                    name_length,
+                    tag_key_length,
+                    tag_value_length,
+                    tags_per_msg,
+                    // TODO -- Validate user input for multivalue_pack_probability.
+                    multivalue_pack_probability,
+                    multivalue_count,
+                    kind_weights,
+                    metric_weights,
+                    value,
+                    service_check_names,
+                },
+            ) => {
+                if !conf.valid() {
+                    return Err(Error::InvalidConfig);
+                }
                 let serializer = crate::DogStatsD::new(
                     *contexts,
+                    *service_check_names,
                     *name_length,
                     *tag_key_length,
                     *tag_value_length,
@@ -185,50 +230,50 @@ impl Cache {
                     *metric_weights,
                     *value,
                     &mut rng,
-                );
+                )?;
 
-                construct_block_cache_inner(&mut rng, &serializer, &block_chunks)
+                construct_block_cache_inner(&mut rng, &serializer, &block_chunks)?
             }
             crate::Config::Fluent => {
                 let pyld = crate::Fluent::new(&mut rng);
-                construct_block_cache_inner(&mut rng, &pyld, &block_chunks)
+                construct_block_cache_inner(&mut rng, &pyld, &block_chunks)?
             }
             crate::Config::SplunkHec { encoding } => construct_block_cache_inner(
                 &mut rng,
                 &crate::SplunkHec::new(*encoding),
                 &block_chunks,
-            ),
+            )?,
             crate::Config::ApacheCommon => {
                 let pyld = crate::ApacheCommon::new(&mut rng);
-                construct_block_cache_inner(&mut rng, &pyld, &block_chunks)
+                construct_block_cache_inner(&mut rng, &pyld, &block_chunks)?
             }
             crate::Config::Ascii => {
                 let pyld = crate::Ascii::new(&mut rng);
-                construct_block_cache_inner(&mut rng, &pyld, &block_chunks)
+                construct_block_cache_inner(&mut rng, &pyld, &block_chunks)?
             }
             crate::Config::DatadogLog => {
                 let serializer = crate::DatadogLog::new(&mut rng);
-                construct_block_cache_inner(&mut rng, &serializer, &block_chunks)
+                construct_block_cache_inner(&mut rng, &serializer, &block_chunks)?
             }
             crate::Config::Json => {
-                construct_block_cache_inner(&mut rng, &crate::Json, &block_chunks)
+                construct_block_cache_inner(&mut rng, &crate::Json, &block_chunks)?
             }
             crate::Config::Static { ref static_path } => construct_block_cache_inner(
                 &mut rng,
                 &crate::Static::new(static_path),
                 &block_chunks,
-            ),
+            )?,
             crate::Config::OpentelemetryTraces => {
                 let pyld = crate::OpentelemetryTraces::new(&mut rng);
-                construct_block_cache_inner(rng, &pyld, &block_chunks)
+                construct_block_cache_inner(rng, &pyld, &block_chunks)?
             }
             crate::Config::OpentelemetryLogs => {
                 let pyld = crate::OpentelemetryLogs::new(&mut rng);
-                construct_block_cache_inner(rng, &pyld, &block_chunks)
+                construct_block_cache_inner(rng, &pyld, &block_chunks)?
             }
             crate::Config::OpentelemetryMetrics => {
                 let pyld = crate::OpentelemetryMetrics::new(&mut rng);
-                construct_block_cache_inner(rng, &pyld, &block_chunks)
+                construct_block_cache_inner(rng, &pyld, &block_chunks)?
             }
         };
         Ok(Self::Fixed { idx: 0, blocks })
@@ -285,21 +330,28 @@ fn stream_inner(
             let pyld = crate::Syslog5424::default();
             stream_block_inner(&mut rng, total_bytes, &pyld, block_chunks, &snd)
         }
-        crate::Config::DogStatsD(crate::dogstatsd::Config {
-            contexts,
-            name_length,
-            tag_key_length,
-            tag_value_length,
-            tags_per_msg,
-            // TODO -- Validate user input for multivalue_pack_probability.
-            multivalue_pack_probability,
-            multivalue_count,
-            kind_weights,
-            metric_weights,
-            value,
-        }) => {
+        crate::Config::DogStatsD(
+            conf @ crate::dogstatsd::Config {
+                contexts,
+                service_check_names,
+                name_length,
+                tag_key_length,
+                tag_value_length,
+                tags_per_msg,
+                // TODO -- Validate user input for multivalue_pack_probability.
+                multivalue_pack_probability,
+                multivalue_count,
+                kind_weights,
+                metric_weights,
+                value,
+            },
+        ) => {
+            if !conf.valid() {
+                return Err(SpinError::InvalidConfig);
+            }
             let pyld = crate::DogStatsD::new(
                 *contexts,
+                *service_check_names,
                 *name_length,
                 *tag_key_length,
                 *tag_value_length,
@@ -310,7 +362,7 @@ fn stream_inner(
                 *metric_weights,
                 *value,
                 &mut rng,
-            );
+            )?;
 
             stream_block_inner(&mut rng, total_bytes, &pyld, block_chunks, &snd)
         }
@@ -443,7 +495,7 @@ fn construct_block_cache_inner<R, S>(
     mut rng: &mut R,
     serializer: &S,
     block_chunks: &[u32],
-) -> Vec<Block>
+) -> Result<Vec<Block>, ConstructBlockCacheError>
 where
     S: crate::Serialize,
     R: Rng + ?Sized,
@@ -454,8 +506,11 @@ where
             block_cache.push(block);
         }
     }
-    assert!(!block_cache.is_empty());
-    block_cache
+    if block_cache.is_empty() {
+        Err(ConstructBlockCacheError::InsufficientBlockSizes)
+    } else {
+        Ok(block_cache)
+    }
 }
 
 #[inline]

lading_payload/Cargo.toml
@@ -23,6 +23,7 @@ thiserror = { workspace = true }
 time = { version = "0.3", features = ["formatting"] }
 tracing = { workspace = true }
 tokio = { workspace = true }
+arbitrary = { version = "1", optional = true, features = ["derive"] }
 
 [dev-dependencies]
 proptest = "1.4"
@@ -31,6 +32,7 @@ criterion = { version = "0.5", features = ["html_reports"] }
 
 [features]
 default = []
+fuzz = ["arbitrary"]
 
 [lib]
 doctest = false

lading_payload/fuzz/.gitignore
@@ -1,0 +1,4 @@
+target
+corpus
+artifacts
+coverage

lading_payload/fuzz/Cargo.toml
@@ -1,0 +1,66 @@
+[package]
+name = "lading-payload-fuzz"
+version = "0.0.0"
+publish = false
+edition = "2021"
+
+[package.metadata]
+cargo-fuzz = true
+
+[dependencies]
+libfuzzer-sys = "0.4"
+arbitrary = { version = "1", features = ["derive"] }
+rand = { version = "0.8", default-features = false, features = ["small_rng", "std", "std_rng" ]}
+
+[dependencies.lading-payload]
+path = ".."
+features = ["fuzz"]
+
+# Prevent this from interfering with workspaces
+[workspace]
+members = ["."]
+
+[profile.release]
+debug = 1
+
+[[bin]]
+name = "fixed_cache_creation"
+path = "fuzz_targets/fixed_cache_creation.rs"
+test = false
+doc = false
+
+[[bin]]
+name = "fixed_cache_creation_fluent"
+path = "fuzz_targets/fixed_cache_creation_fluent.rs"
+test = false
+doc = false
+
+[[bin]]
+name = "fixed_cache_creation_apache_common"
+path = "fuzz_targets/fixed_cache_creation_apache_common.rs"
+test = false
+doc = false
+
+[[bin]]
+name = "fixed_cache_creation_ascii"
+path = "fuzz_targets/fixed_cache_creation_ascii.rs"
+test = false
+doc = false
+
+[[bin]]
+name = "fixed_cache_creation_dogstatsd"
+path = "fuzz_targets/fixed_cache_creation_dogstatsd.rs"
+test = false
+doc = false
+
+[[bin]]
+name = "fixed_cache_creation_datadog_log"
+path = "fuzz_targets/fixed_cache_creation_datadog_log.rs"
+test = false
+doc = false
+
+[[bin]]
+name = "fixed_cache_creation_json"
+path = "fuzz_targets/fixed_cache_creation_json.rs"
+test = false
+doc = false

lading_payload/fuzz/fuzz_targets/fixed_cache_creation.rs
@@ -1,0 +1,32 @@
+#![no_main]
+
+use arbitrary;
+use libfuzzer_sys::fuzz_target;
+use rand::{rngs::SmallRng, SeedableRng};
+use std::num::NonZeroU32;
+
+use lading_payload::block::Cache;
+
+#[derive(arbitrary::Arbitrary, Debug)]
+struct Input {
+    seed: [u8; 32],
+    total_bytes: NonZeroU32,
+    block_bytes_sizes: [NonZeroU32; 32],
+    payload: lading_payload::Config,
+}
+
+fuzz_target!(|input: Input| {
+    for byte_size in &input.block_bytes_sizes {
+        if byte_size > &input.total_bytes {
+            return;
+        }
+    }
+
+    let mut rng = SmallRng::from_seed(input.seed);
+    let _res = Cache::fixed(
+        &mut rng,
+        input.total_bytes,
+        &input.block_bytes_sizes,
+        &input.payload,
+    );
+});

lading_payload/fuzz/fuzz_targets/fixed_cache_creation_apache_common.rs
@@ -1,0 +1,31 @@
+#![no_main]
+
+use arbitrary;
+use libfuzzer_sys::fuzz_target;
+use rand::{rngs::SmallRng, SeedableRng};
+use std::num::NonZeroU32;
+
+use lading_payload::block::Cache;
+
+#[derive(arbitrary::Arbitrary, Debug)]
+struct Input {
+    seed: [u8; 32],
+    total_bytes: NonZeroU32,
+    block_bytes_sizes: [NonZeroU32; 32],
+}
+
+fuzz_target!(|input: Input| {
+    for byte_size in &input.block_bytes_sizes {
+        if byte_size > &input.total_bytes || byte_size.get() > 10_000_000 {
+            return;
+        }
+    }
+
+    let mut rng = SmallRng::from_seed(input.seed);
+    let _res = Cache::fixed(
+        &mut rng,
+        input.total_bytes,
+        &input.block_bytes_sizes,
+        &lading_payload::Config::ApacheCommon,
+    );
+});

lading_payload/fuzz/fuzz_targets/fixed_cache_creation_ascii.rs
@@ -1,0 +1,31 @@
+#![no_main]
+
+use arbitrary;
+use libfuzzer_sys::fuzz_target;
+use rand::{rngs::SmallRng, SeedableRng};
+use std::num::NonZeroU32;
+
+use lading_payload::block::Cache;
+
+#[derive(arbitrary::Arbitrary, Debug)]
+struct Input {
+    seed: [u8; 32],
+    total_bytes: NonZeroU32,
+    block_bytes_sizes: [NonZeroU32; 32],
+}
+
+fuzz_target!(|input: Input| {
+    for byte_size in &input.block_bytes_sizes {
+        if byte_size > &input.total_bytes || byte_size.get() > 10_000_000 {
+            return;
+        }
+    }
+
+    let mut rng = SmallRng::from_seed(input.seed);
+    let _res = Cache::fixed(
+        &mut rng,
+        input.total_bytes,
+        &input.block_bytes_sizes,
+        &lading_payload::Config::Ascii,
+    );
+});

lading_payload/fuzz/fuzz_targets/fixed_cache_creation_datadog_log.rs
@@ -1,0 +1,31 @@
+#![no_main]
+
+use arbitrary;
+use libfuzzer_sys::fuzz_target;
+use rand::{rngs::SmallRng, SeedableRng};
+use std::num::NonZeroU32;
+
+use lading_payload::block::Cache;
+
+#[derive(arbitrary::Arbitrary, Debug)]
+struct Input {
+    seed: [u8; 32],
+    total_bytes: NonZeroU32,
+    block_bytes_sizes: [NonZeroU32; 32],
+}
+
+fuzz_target!(|input: Input| {
+    for byte_size in &input.block_bytes_sizes {
+        if byte_size > &input.total_bytes || byte_size.get() > 10_000_000 {
+            return;
+        }
+    }
+
+    let mut rng = SmallRng::from_seed(input.seed);
+    let _res = Cache::fixed(
+        &mut rng,
+        input.total_bytes,
+        &input.block_bytes_sizes,
+        &lading_payload::Config::DatadogLog,
+    );
+});

commit f01591ed40d25600777cd9a6ef2ff83cc2f83a7b

Adjust static generator to not panic
Discovered by the fuzzing work introduced in #727 we do not want to panic in the
constructor for the static generator but instead kick out errors.

REF SMPTNG-79

Signed-off-by: Brian L. Troutwine <brian.troutwine@datadoghq.com>

lading_payload/src/block.rs
@@ -28,10 +28,13 @@ pub enum SpinError {
     /// DogStatsD creation error
     #[error(transparent)]
     DogStatsD(#[from] dogstatsd::Error),
+    /// Static payload creation error
+    #[error(transparent)]
+    Static(#[from] crate::statik::Error),
 }
 
 /// Error for [`Cache`]
-#[derive(Debug, thiserror::Error, Clone, Copy)]
+#[derive(Debug, thiserror::Error)]
 pub enum Error {
     /// See [`ChunkError`]
     #[error("Chunk error: {0}")]
@@ -45,6 +48,9 @@ pub enum Error {
     /// DogStatsD creation error
     #[error(transparent)]
     DogStatsD(#[from] dogstatsd::Error),
+    /// Static payload creation error
+    #[error(transparent)]
+    Static(#[from] crate::statik::Error),
 }
 
 /// Errors for the construction of chunks
@@ -260,7 +266,7 @@ impl Cache {
             }
             crate::Config::Static { ref static_path } => construct_block_cache_inner(
                 &mut rng,
-                &crate::Static::new(static_path),
+                &crate::Static::new(static_path)?,
                 &block_chunks,
             )?,
             crate::Config::OpentelemetryTraces => {
@@ -391,7 +397,7 @@ fn stream_inner(
             stream_block_inner(&mut rng, total_bytes, &pyld, block_chunks, &snd)
         }
         crate::Config::Static { ref static_path } => {
-            let pyld = crate::Static::new(static_path);
+            let pyld = crate::Static::new(static_path)?;
             stream_block_inner(&mut rng, total_bytes, &pyld, block_chunks, &snd)
         }
         crate::Config::OpentelemetryTraces => {

lading_payload/src/statik.rs
@@ -9,8 +9,6 @@ use std::{
 use rand::{prelude::IteratorRandom, Rng};
 use tracing::debug;
 
-use crate::Error;
-
 #[derive(Debug)]
 struct Source {
     byte_size: u64,
@@ -23,18 +21,25 @@ pub struct Static {
     sources: Vec<Source>,
 }
 
+#[derive(thiserror::Error, Debug)]
+/// Errors produced by [`Static`].
+pub enum Error {
+    /// IO error
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+}
+
 impl Static {
-    #[must_use]
     /// Create a new instance of `Static`
     ///
-    /// # Panics
+    /// # Errors
     ///
-    /// Will panic if `path` is not a valid directory or file.
-    pub fn new(path: &Path) -> Self {
+    /// See documentation for [`Error`]
+    pub fn new(path: &Path) -> Result<Self, Error> {
         let mut sources = Vec::with_capacity(16);
 
         // Attempt to open the path, if this fails we assume that it is a directory.
-        let metadata = fs::metadata(path).unwrap();
+        let metadata = fs::metadata(path)?;
         if metadata.is_file() {
             debug!("Static path {} is a file.", path.display());
             let byte_size = metadata.len();
@@ -44,12 +49,12 @@ impl Static {
             });
         } else if metadata.is_dir() {
             debug!("Static path {} is a directory.", path.display());
-            for entry in fs::read_dir(path).expect("could not read directory") {
-                let entry = entry.unwrap();
+            for entry in fs::read_dir(path)? {
+                let entry = entry?;
                 let entry_pth = entry.path();
                 debug!("Attempting to open {} as file.", entry_pth.display());
                 if let Ok(file) = std::fs::OpenOptions::new().read(true).open(&entry_pth) {
-                    let byte_size = file.metadata().expect("could not read file metadata").len();
+                    let byte_size = file.metadata()?.len();
                     sources.push(Source {
                         byte_size,
                         path: entry_pth.clone(),
@@ -58,12 +63,17 @@ impl Static {
             }
         }
 
-        Self { sources }
+        Ok(Self { sources })
     }
 }
 
 impl crate::Serialize for Static {
-    fn to_bytes<W, R>(&self, mut rng: R, max_bytes: usize, writer: &mut W) -> Result<(), Error>
+    fn to_bytes<W, R>(
+        &self,
+        mut rng: R,
+        max_bytes: usize,
+        writer: &mut W,
+    ) -> Result<(), crate::Error>
     where
         R: Rng + Sized,
         W: Write,