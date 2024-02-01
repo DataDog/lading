a7fbf993e63d99407489a48f16f69e429f94bbf8
Daniel Malis <daniel.malis@datadoghq.com>
2024-02-01 14:41:43 -0500

    Converted more expects

--- lading/src/generator/grpc.rs
+++ lading/src/generator/grpc.rs
@@ -52,6 +52,9 @@ pub enum Error {
     /// Byte error
     #[error("Bytes must not be negative: {0}")]
     Byte(#[from] ByteError),
+    /// Zero value
+    #[error("Value cannot be zero")]
+    Zero,
 }
 
 /// Config for [`Grpc`]
@@ -188,8 +191,8 @@ impl Grpc {
             labels.push(("id".to_string(), id));
         }
 
-        let bytes_per_second = NonZeroU32::new(config.bytes_per_second.get_bytes() as u32)
-            .expect("configu bytes per second must be non-zero");
+        let bytes_per_second =
+            NonZeroU32::new(config.bytes_per_second.get_bytes() as u32).ok_or(Error::Zero)?;
         gauge!(
             "bytes_per_second",
             f64::from(bytes_per_second.get()),
@@ -198,7 +201,7 @@ impl Grpc {
 
         let total_bytes =
             NonZeroU32::new(config.maximum_prebuild_cache_size_bytes.get_bytes() as u32)
-                .expect("bytes must be non-zero");
+                .ok_or(Error::Zero)?;
         let block_cache = match config.block_cache_method {
             block::CacheMethod::Streaming => block::Cache::stream(
                 config.seed,
