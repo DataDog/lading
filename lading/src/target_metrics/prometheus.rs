f89e7dc7993b423376f491facebfc26332a5794b
Scott Opell <me@scottopell.com>
2024-03-22 20:23:55 +0000

    Adds skeleton of tests for prometheus scraping

--- lading/src/target_metrics/prometheus.rs
+++ lading/src/target_metrics/prometheus.rs
@@ -96,137 +96,14 @@ impl Prometheus {
     #[allow(clippy::cast_possible_truncation)]
     #[allow(clippy::too_many_lines)]
     pub(crate) async fn run(mut self) -> Result<(), Error> {
-        let client = reqwest::Client::new();
-
-        let server = async move {
+        let mut shutdown = self.shutdown.clone();
+        let server = async {
             info!("Prometheus target metrics scraper running, but waiting for warmup to complete");
             self.experiment_started.recv().await; // block until experimental phase entered
             info!("Prometheus target metrics scraper starting collection");
             loop {
                 tokio::time::sleep(Duration::from_secs(1)).await;
-                let Ok(resp) = client
-                    .get(&self.config.uri)
-                    .timeout(Duration::from_secs(1))
-                    .send()
-                    .await
-                else {
-                    info!("failed to get Prometheus uri");
-                    continue;
-                };
-
-                let Ok(text) = resp.text().await else {
-                    info!("failed to read Prometheus response");
-                    continue;
-                };
-
-                // remember the type for each metric across lines
-                let mut typemap = FxHashMap::default();
-
-                // this deserves a real parser, but this will do for now.
-                // Format doc: https://github.com/prometheus/docs/blob/main/content/docs/instrumenting/exposition_formats.md
-                for line in text.lines().filter(|l| !l.is_empty()) {
-                    if line.starts_with("# HELP") {
-                        continue;
-                    }
-
-                    if line.starts_with("# TYPE") {
-                        let mut parts = line.split_ascii_whitespace().skip(2);
-                        let name = parts.next().expect("parts iterator is missing name");
-                        let metric_type =
-                            parts.next().expect("parts iterator is missing metric type");
-                        let metric_type: MetricType =
-                            metric_type.parse().expect("failed to parse metric type");
-                        // summary and histogram metrics additionally report names suffixed with _sum, _count, _bucket
-                        if matches!(metric_type, MetricType::Histogram | MetricType::Summary) {
-                            typemap.insert(format!("{name}_sum"), metric_type);
-                            typemap.insert(format!("{name}_count"), metric_type);
-                            typemap.insert(format!("{name}_bucket"), metric_type);
-                        }
-                        typemap.insert(name.to_owned(), metric_type);
-                        continue;
-                    }
-
-                    let mut parts = line.split_ascii_whitespace();
-                    let name_and_labels = parts
-                        .next()
-                        .expect("parts iterator is missing name and labels");
-                    let value = parts.next().expect("parts iterator is missing value");
-
-                    if value.contains('#') {
-                        trace!("Unknown format: {value}");
-                        continue;
-                    }
-
-                    let (name, labels) = {
-                        if let Some((name, labels)) = name_and_labels.split_once('{') {
-                            let labels = labels.trim_end_matches('}');
-                            let labels = labels.split(',').map(|label| {
-                                let (label_name, label_value) = label
-                                    .split_once('=')
-                                    .expect("label failed to split on first =");
-                                let label_value = label_value.trim_matches('\"');
-                                (label_name.to_owned(), label_value.to_owned())
-                            });
-                            let labels = labels.collect::<Vec<_>>();
-                            (name, Some(labels))
-                        } else {
-                            (name_and_labels, None)
-                        }
-                    };
-
-                    let metric_type = typemap.get(name);
-                    let name = name.replace("__", ".");
-
-                    if let Some(metrics) = &self.config.metrics {
-                        if !metrics.contains(&name) {
-                            continue;
-                        }
-                    }
-
-                    match metric_type {
-                        Some(MetricType::Gauge) => {
-                            let Ok(value): Result<f64, _> = value.parse() else {
-                                let e = value.parse::<f64>().expect("failed to parse gauge value");
-                                warn!("{e}: {name} = {value}");
-                                continue;
-                            };
-
-                            trace!("gauge: {name} = {value}");
-                            gauge!(format!("target/{name}"), value, &labels.unwrap_or_default());
-                        }
-                        Some(MetricType::Counter) => {
-                            let Ok(value): Result<f64, _> = value.parse() else {
-                                let e =
-                                    value.parse::<f64>().expect("failed to parse counter value");
-                                warn!("{e}: {name} = {value}");
-                                continue;
-                            };
-
-                            let value = if value < 0.0 {
-                                warn!("Negative counter value unhandled");
-                                continue;
-                            } else {
-                                // clippy shows "error: casting `f64` to `u64` may lose the sign of the value". This is
-                                // guarded by the sign check above.
-                                if value > u64::MAX as f64 {
-                                    warn!("Counter value above maximum limit");
-                                    continue;
-                                }
-                                value as u64
-                            };
-
-                            trace!("counter: {name} = {value}");
-                            absolute_counter!(
-                                format!("target/{name}"),
-                                value,
-                                &labels.unwrap_or_default()
-                            );
-                        }
-                        Some(_) | None => {
-                            trace!("unsupported metric type: {name} = {value}");
-                        }
-                    }
-                }
+                self.scrape_metrics().await;
             }
         };
 
@@ -235,10 +112,180 @@ impl Prometheus {
                 error!("server shutdown unexpectedly");
                  Err(Error::EarlyShutdown)
             }
-            () = self.shutdown.recv() => {
+            () = shutdown.recv() => {
                 info!("shutdown signal received");
                  Ok(())
             }
         }
+    }
+
+    #[allow(clippy::too_many_lines)]
+    async fn scrape_metrics(&self) {
+        let client = reqwest::Client::new();
+        let Ok(resp) = client
+            .get(&self.config.uri)
+            .timeout(Duration::from_secs(1))
+            .send()
+            .await
+        else {
+            info!("failed to get Prometheus uri");
+            return;
+        };
+
+        let Ok(text) = resp.text().await else {
+            info!("failed to read Prometheus response");
+            return;
+        };
+
+        // remember the type for each metric across lines
+        let mut typemap = FxHashMap::default();
+
+        // this deserves a real parser, but this will do for now.
+        // Format doc: https://github.com/prometheus/docs/blob/main/content/docs/instrumenting/exposition_formats.md
+        for line in text.lines().filter(|l| !l.is_empty()) {
+            if line.starts_with("# HELP") {
+                continue;
+            }
+
+            if line.starts_with("# TYPE") {
+                let mut parts = line.split_ascii_whitespace().skip(2);
+                let name = parts.next().expect("parts iterator is missing name");
+                let metric_type = parts.next().expect("parts iterator is missing metric type");
+                let metric_type: MetricType =
+                    metric_type.parse().expect("failed to parse metric type");
+                // summary and histogram metrics additionally report names suffixed with _sum, _count, _bucket
+                if matches!(metric_type, MetricType::Histogram | MetricType::Summary) {
+                    typemap.insert(format!("{name}_sum"), metric_type);
+                    typemap.insert(format!("{name}_count"), metric_type);
+                    typemap.insert(format!("{name}_bucket"), metric_type);
+                }
+                typemap.insert(name.to_owned(), metric_type);
+                continue;
+            }
+
+            let mut parts = line.split_ascii_whitespace();
+            let name_and_labels = parts
+                .next()
+                .expect("parts iterator is missing name and labels");
+            let value = parts.next().expect("parts iterator is missing value");
+
+            if value.contains('#') {
+                trace!("Unknown format: {value}");
+                continue;
+            }
+
+            let (name, labels) = {
+                if let Some((name, labels)) = name_and_labels.split_once('{') {
+                    let labels = labels.trim_end_matches('}');
+                    let labels = labels.split(',').map(|label| {
+                        let (label_name, label_value) = label
+                            .split_once('=')
+                            .expect("label failed to split on first =");
+                        let label_value = label_value.trim_matches('\"');
+                        (label_name.to_owned(), label_value.to_owned())
+                    });
+                    let labels = labels.collect::<Vec<_>>();
+                    (name, Some(labels))
+                } else {
+                    (name_and_labels, None)
+                }
+            };
+
+            let metric_type = typemap.get(name);
+            let name = name.replace("__", ".");
+
+            if let Some(metrics) = &self.config.metrics {
+                if !metrics.contains(&name) {
+                    continue;
+                }
+            }
+
+            match metric_type {
+                Some(MetricType::Gauge) => {
+                    let Ok(value): Result<f64, _> = value.parse() else {
+                        let e = value.parse::<f64>().expect("failed to parse gauge value");
+                        warn!("{e}: {name} = {value}");
+                        continue;
+                    };
+
+                    trace!("gauge: {name} = {value}");
+                    gauge!(format!("target/{name}"), value, &labels.unwrap_or_default());
+                }
+                Some(MetricType::Counter) => {
+                    let Ok(value): Result<f64, _> = value.parse() else {
+                        let e = value.parse::<f64>().expect("failed to parse counter value");
+                        warn!("{e}: {name} = {value}");
+                        continue;
+                    };
+
+                    let value = if value < 0.0 {
+                        warn!("Negative counter value unhandled");
+                        continue;
+                    } else {
+                        // clippy shows "error: casting `f64` to `u64` may lose the sign of the value". This is
+                        // guarded by the sign check above.
+                        if value > u64::MAX as f64 {
+                            warn!("Counter value above maximum limit");
+                            continue;
+                        }
+                        value as u64
+                    };
+
+                    trace!("counter: {name} = {value}");
+                    absolute_counter!(format!("target/{name}"), value, &labels.unwrap_or_default());
+                }
+                Some(_) | None => {
+                    trace!("unsupported metric type: {name} = {value}");
+                }
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use warp;
+    use warp::Filter;
+
+    #[tokio::test]
+    async fn test_scrape_and_parse_metrics() {
+        // Create a mock http server to serve Prometheus metrics
+        let server = warp::serve(warp::path("metrics").map(|| {
+            warp::reply::with_status(
+                "target_metric{label1=\"value1\", label2=\"value2\"} 42.0\n",
+                warp::http::StatusCode::OK,
+            )
+        }));
+
+        let (addr, serve_fut) = server.bind_ephemeral(([127, 0, 0, 1], 0));
+        let _server_handle = tokio::spawn(serve_fut);
+
+        let shutdown = Phase::new();
+        let experiment_started = Phase::new();
+        let server_uri = format!("http://{addr}/metrics");
+        let p = Prometheus::new(
+            Config {
+                uri: server_uri,
+                metrics: None,
+            },
+            shutdown.clone(),
+            experiment_started.clone(),
+        );
+
+        let dr = metrics_util::debugging::DebuggingRecorder::new();
+        let snapshotter = dr.snapshotter();
+        dr.install().expect("failed to install recorder");
+
+        experiment_started.signal();
+
+        p.scrape_metrics().await;
+
+        let snapshot = snapshotter.snapshot().into_vec();
+
+        // todo, better mock data and actual tests
+        assert_eq!(snapshot.len(), 1);
+
+        shutdown.signal();
     }
 }
