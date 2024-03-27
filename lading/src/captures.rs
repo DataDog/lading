f282c17e044d1710734e03dd9f91594830296728
Geoffrey M. Oxberry <geoffrey.oxberry@datadoghq.com>
2024-03-27 11:08:10 -0700

    capture_manager: avoid overflow via saturating sub
    When `now.elapsed()` is greater than 1 second In the capture manager's
    call to `std::thread::sleep`, that call will panic with an
    overflow. Using a saturating subtraction should clamp negative values
    to zero, thus preventing such an overflow.
    
    Signed-off-by: Geoffrey M. Oxberry <geoffrey.oxberry@datadoghq.com>

--- lading/src/captures.rs
+++ lading/src/captures.rs
@@ -207,7 +207,7 @@ impl CaptureManager {
                 }
                 // Sleep for 1 second minus however long we just spent recording captures
                 // assumption here is that the time spent recording captures is consistent
-                std::thread::sleep(Duration::from_secs(1) - now.elapsed());
+                std::thread::sleep(Duration::from_secs(1).saturating_sub(now.elapsed()));
             })?;
         Ok(())
     }
