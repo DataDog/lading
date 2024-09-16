180c72694006d087c5c21ab1da7cd31ec9e0db95
Brian L. Troutwine <brian.troutwine@datadoghq.com>
2024-09-16 13:30:35 -0400

    Remove lading_capture protobuf export
    This commit removes the protobuf version of the lading capture. It is unused
    today.
    
    Signed-off-by: Brian L. Troutwine <brian.troutwine@datadoghq.com>

--- lading_capture/src/proto.rs
+++
@@ -1,9 +0,0 @@
-//! Protobuf generated structs
-
-/// Protobuf form of a Lading capture payload. Meant for transmission and not
-/// archival.
-pub mod lading {
-    pub mod v1 {
-        include!("proto/lading_capture.proto.capture.v1.rs");
-    }
-}
