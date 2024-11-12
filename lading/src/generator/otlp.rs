30e4027a2dd4c269c6fce84e93ffe13127be49ad
Dustin Long <dustin.long@datadoghq.com>
2024-11-11 19:36:20 -0500

    Example OTLP traffic generator

--- lading/src/generator/otlp.rs
+++ lading/src/generator/otlp.rs
@@ -2,7 +2,16 @@
 
 use super::General;
 use hyper::Uri;
+use opentelemetry::global;
+use opentelemetry::KeyValue;
+use opentelemetry_otlp;
+use opentelemetry_otlp::WithExportConfig;
+use opentelemetry_sdk::metrics::{PeriodicReader, SdkMeterProvider};
+use opentelemetry_sdk::{runtime, Resource};
 use serde::{Deserialize, Serialize};
+//use opentelemetry_sdk::metrics::data::Temporality;
+use opentelemetry_sdk::metrics::reader::DefaultTemporalitySelector;
+use std::vec;
 
 /// Config for [`OTLP`]
 #[derive(Debug, Deserialize, Serialize, PartialEq)]
@@ -26,15 +35,43 @@ pub struct Config {
     pub throttle: lading_throttle::Config,
 }
 
-#[derive(Copy, Clone, thiserror::Error, Debug)]
+#[allow(missing_docs)]
+#[derive(thiserror::Error, Copy, Clone, Debug)]
 /// Errors produced by [`Otlp`].
-pub enum Error {}
+pub enum Error {
+    // Generic error for OTLP
+    #[error("OTLP Error")]
+    Generic,
+}
 
 /// The OTLP generator.
 ///
 /// This generator is reposnsible for connecting to the target via OTLP.
-#[derive(Copy, Clone, Debug)]
-pub struct Otlp {}
+#[derive(Clone, Debug)]
+pub struct Otlp {
+    target_uri: Uri,
+    shutdown: lading_signal::Watcher,
+}
+
+fn init_meter_provider(endpoint: &str) -> opentelemetry_sdk::metrics::SdkMeterProvider {
+    let exporter = opentelemetry_otlp::new_exporter()
+        .tonic() // create GRPC layer
+        .with_endpoint(endpoint)
+        .build_metrics_exporter(Box::new(DefaultTemporalitySelector::new()))
+        .unwrap();
+
+    let reader = PeriodicReader::builder(exporter, runtime::Tokio).build();
+    let provider = SdkMeterProvider::builder()
+        .with_reader(reader)
+        .with_resource(Resource::new([KeyValue::new(
+            "service.name",
+            "metrics-basic-example",
+        )]))
+        .build();
+
+    global::set_meter_provider(provider.clone());
+    provider
+}
 
 impl Otlp {
     /// Create a new [`Otlp`] instance
@@ -50,10 +87,13 @@ impl Otlp {
     #[allow(clippy::cast_possible_truncation)]
     pub fn new(
         _general: General,
-        _config: Config,
-        _shutdown: lading_signal::Watcher,
+        config: Config,
+        shutdown: lading_signal::Watcher,
     ) -> Result<Self, Error> {
-        Ok(Self {})
+        Ok(Self {
+            target_uri: config.target_uri,
+            shutdown,
+        })
     }
 
     /// Run [`Otlp`] to completion or until a shutdown signal is received.
@@ -63,6 +103,124 @@ impl Otlp {
     /// TODO
     ///
     pub async fn spin(self) -> Result<(), Error> {
-        return Ok(());
+        let meter_provider = init_meter_provider(&self.target_uri.to_string());
+
+        // Create a meter from the above MeterProvider.
+        let meter = global::meter("mylibraryname");
+
+        // Create a Counter Instrument.
+        let counter = meter.u64_counter("my_counter").init();
+
+        // Record measurements using the Counter instrument.
+        counter.add(
+            10,
+            &[
+                KeyValue::new("mykey1", "myvalue1"),
+                KeyValue::new("mykey2", "myvalue2"),
+            ],
+        );
+
+        // Create a ObservableCounter instrument and register a callback that reports the measurement.
+        let _observable_counter = meter
+            .u64_observable_counter("my_observable_counter")
+            .with_description("My observable counter example description")
+            .with_unit("myunit")
+            .with_callback(|observer| {
+                observer.observe(
+                    100,
+                    &[
+                        KeyValue::new("mykey1", "myvalue1"),
+                        KeyValue::new("mykey2", "myvalue2"),
+                    ],
+                )
+            })
+            .init();
+
+        // Create a UpCounter Instrument.
+        let updown_counter = meter.i64_up_down_counter("my_updown_counter").init();
+
+        // Record measurements using the UpCounter instrument.
+        updown_counter.add(
+            -10,
+            &[
+                KeyValue::new("mykey1", "myvalue1"),
+                KeyValue::new("mykey2", "myvalue2"),
+            ],
+        );
+
+        // Create a Observable UpDownCounter instrument and register a callback that reports the measurement.
+        let _observable_up_down_counter = meter
+            .i64_observable_up_down_counter("my_observable_updown_counter")
+            .with_description("My observable updown counter example description")
+            .with_unit("myunit")
+            .with_callback(|observer| {
+                observer.observe(
+                    100,
+                    &[
+                        KeyValue::new("mykey1", "myvalue1"),
+                        KeyValue::new("mykey2", "myvalue2"),
+                    ],
+                )
+            })
+            .init();
+
+        // Create a Histogram Instrument.
+        let histogram = meter
+            .f64_histogram("my_histogram")
+            .with_description("My histogram example description")
+            // Setting boundaries is optional. By default, the boundaries are set to
+            // [0.0, 5.0, 10.0, 25.0, 50.0, 75.0, 100.0, 250.0, 500.0, 750.0, 1000.0, 2500.0, 5000.0, 7500.0, 10000.0]
+            .with_boundaries(vec![0.0, 5.0, 10.0, 15.0, 20.0, 25.0])
+            .init();
+
+        // Record measurements using the histogram instrument.
+        histogram.record(
+            10.5,
+            &[
+                KeyValue::new("mykey1", "myvalue1"),
+                KeyValue::new("mykey2", "myvalue2"),
+            ],
+        );
+
+        // Note that there is no ObservableHistogram instrument.
+
+        // Create a Gauge Instrument.
+        let gauge = meter
+            .f64_gauge("my_gauge")
+            .with_description("A gauge set to 1.0")
+            .with_unit("myunit")
+            .init();
+
+        gauge.record(
+            1.0,
+            &[
+                KeyValue::new("mykey1", "myvalue1"),
+                KeyValue::new("mykey2", "myvalue2"),
+            ],
+        );
+
+        // Create a ObservableGauge instrument and register a callback that reports the measurement.
+        let _observable_gauge = meter
+            .f64_observable_gauge("my_observable_gauge")
+            .with_description("An observable gauge set to 1.0")
+            .with_unit("myunit")
+            .with_callback(|observer| {
+                observer.observe(
+                    1.0,
+                    &[
+                        KeyValue::new("mykey1", "myvalue1"),
+                        KeyValue::new("mykey2", "myvalue2"),
+                    ],
+                )
+            })
+            .init();
+
+        // Metrics are exported by default every 30 seconds when using stdout exporter,
+        // however shutting down the MeterProvider here instantly flushes
+        // the metrics, instead of waiting for the 30 sec interval.
+        match meter_provider.shutdown() {
+            Ok(_) => return Ok(()),
+            Err(_err) => return Err(Error::Generic),
+        }
     }
 }
