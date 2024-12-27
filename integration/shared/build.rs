6f7f043d0de41826b39d2909e54289c6cd47e7c4
Scott Opell <me@scottopell.com>
2024-12-27 15:44:43 -0500

    tonic comes along too

--- integration/shared/build.rs
+++ integration/shared/build.rs
@@ -1,6 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(true)
-        .compile(&["proto/integration_api.proto"], &["proto"])?;
+        .compile_protos(&["proto/integration_api.proto"], &["proto"])?;
     Ok(())
 }
