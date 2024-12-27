2394ad78f44a79da6626058c2509e312f3f8fba1
Brian L. Troutwine <brian.troutwine@datadoghq.com>
2024-12-27 11:24:02 -0800

    checkpoint
    Signed-off-by: Brian L. Troutwine <brian.troutwine@datadoghq.com>

--- lading/src/codec.rs
+++ lading/src/codec.rs
@@ -2,7 +2,8 @@ use std::io::Read;
 
 use bytes::{Buf, Bytes};
 use flate2::read::{MultiGzDecoder, ZlibDecoder};
-use hyper::{Body, StatusCode};
+use http_body_util::combinators::BoxBody;
+use hyper::StatusCode;
 
 /// decode decodes a HTTP request body based on its Content-Encoding header.
 /// Only identity, gzip, and deflate are currently supported content encodings.
@@ -24,7 +25,7 @@ use hyper::{Body, StatusCode};
 pub(crate) fn decode(
     content_encoding: Option<&hyper::header::HeaderValue>,
     mut body: Bytes,
-) -> Result<Bytes, hyper::Response<hyper::Body>> {
+) -> Result<Bytes, hyper::Response<BoxBody<Bytes, hyper::Error>>> {
     if let Some(content_encoding) = content_encoding {
         let content_encoding = String::from_utf8_lossy(content_encoding.as_bytes());
 
@@ -61,7 +62,7 @@ pub(crate) fn decode(
                 encoding => {
                     return Err(hyper::Response::builder()
                         .status(StatusCode::UNSUPPORTED_MEDIA_TYPE)
-                        .body(hyper::Body::from(format!(
+                        .body(crate::full(format!(
                             "Unsupported encoding type: {encoding}"
                         )))
                         .expect("failed to build response"))
@@ -76,10 +77,10 @@ pub(crate) fn decode(
 fn encoding_error_to_response(
     encoding: &str,
     error: impl std::error::Error,
-) -> hyper::Response<Body> {
+) -> hyper::Response<BoxBody<Bytes, hyper::Error>> {
     hyper::Response::builder()
         .status(StatusCode::UNSUPPORTED_MEDIA_TYPE)
-        .body(hyper::Body::from(format!(
+        .body(crate::full(format!(
             "failed to decode input as {encoding}: {error}"
         )))
         .expect("failed to build response")
