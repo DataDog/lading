d28eb28d7f2c85a57784ae391e1b201a5898567a
Brian L. Troutwine <brian.troutwine@datadoghq.com>
2025-03-28 10:21:48 -0700

    fix logic, change names
    Signed-off-by: Brian L. Troutwine <brian.troutwine@datadoghq.com>

--- lading/src/observer/linux/pidfd.rs
+++ lading/src/observer/linux/pidfd.rs
@@ -50,22 +50,68 @@ impl PidFd {
     /// without waiting. Returns true immediately if the process has exited,
     /// else false.
     #[allow(clippy::cast_sign_loss)]
-    pub(crate) fn is_active(&self) -> Result<bool, Error> {
+    pub(crate) fn is_exited(&self) -> Result<bool, Error> {
         let mut info = siginfo();
 
         // waitid(P_PIDFD, <fd>, &mut info, WEXITED|WNOHANG)
-        Errno::result(unsafe {
+        //
+        // WEXITED -- Wait for processes that have exited.
+        //
+        // WNOHANG -- Do not hang if no status is available; return
+        //
+        // If waitid() returns because a child process was found that satisfied
+        // the conditions indicated by the arguments idtype and options, then
+        // the structure pointed to by infop shall be filled in by the system
+        // with the status of the process; the si_signo member shall be set
+        // equal to SIGCHLD.  If waitid() returns because WNOHANG was specified
+        // and status is not available for any process specified by idtype and
+        // id, then the si_signo and si_pid members of the structure pointed to
+        // by infop shall be set to zero and the values of other members of the
+        // structure are unspecified.
+        //
+        // The waitid() function shall fail if:
+        //
+        // ECHILD -- The calling process has no existing unwaited-for child
+        // processes.
+        //
+        // EINTR -- The waitid() function was interrupted by a signal.
+        //
+        // EINVAL -- An invalid value was specified for options, or idtype and
+        // id specify an invalid set of processes.
+        //
+        // With pidfd in play ECHILD will be returned if the target process has
+        // exited, didn't exist to begin with or lading is not in the same pid
+        // namespace.
+
+        let res = Errno::result(unsafe {
             libc::waitid(
                 libc::P_PIDFD,
-                self.fd as libc::id_t, // 'pid' in waitid terms
+                self.fd as libc::id_t,
                 &raw mut info,
                 libc::WEXITED | libc::WNOHANG,
             )
-        })?;
+        });
 
-        // If `info.si_pid() != 0`, that means the process has exited.
-        let exited = unsafe { info.si_pid() } != 0;
-        Ok(exited)
+        // If WNOHANG was specified and status is not available for any process
+        // specified by idtype and id, 0 shall be returned. If waitid() returns
+        // due to the change of state of one of its children, 0 shall be
+        // returned. Otherwise, -1 shall be returned and errno set to indicate
+        // the error.
+
+        match res {
+            Ok(0) => {
+                let si_pid = unsafe { info.si_pid() };
+                // NOTE `info` lacks a si_signo and it's sufficient to check
+                // only si_pid.
+                Ok(si_pid != 0)
+            }
+            Ok(_) => unreachable!("waitid() returned a value other than 0 or -1"),
+            Err(Errno::ECHILD) => {
+                // No such process existed or remains.
+                Ok(true)
+            }
+            Err(e) => Err(e),
+        }
     }
 }
 
