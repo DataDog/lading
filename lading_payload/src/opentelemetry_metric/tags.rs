8daf3f333227d7922c1127579bb433c6bb8352a0
Brian L. Troutwine <brian.troutwine@datadoghq.com>
2025-05-13 16:00:52 -0700

    allow tagsgenerator to calculate budget overhead correctly
    Signed-off-by: Brian L. Troutwine <brian.troutwine@datadoghq.com>

--- lading_payload/src/opentelemetry_metric/tags.rs
+++ lading_payload/src/opentelemetry_metric/tags.rs
@@ -1,5 +1,5 @@
 //! Tag generation for OpenTelemetry metric payloads
-use std::rc::Rc;
+use std::{cmp, rc::Rc};
 
 use super::templates::GeneratorError;
 use crate::{Error, Generator, common::config::ConfRange, common::strings::Pool};
@@ -10,6 +10,10 @@ use prost::Message;
 pub(crate) struct TagGenerator {
     inner: crate::common::tags::Generator,
 }
+
+// smallest useful protobuf, determined by experimentation and enforced in
+// smallest_kv_protobuf test
+const SMALLEST_KV_PROTOBUF: usize = 10;
 
 impl TagGenerator {
     /// Creates a new tag generator
@@ -39,6 +43,23 @@ impl TagGenerator {
     }
 }
 
+fn varint_len(v: usize) -> usize {
+    let mut v = v;
+    let mut n = 1;
+    while v > 0x7f {
+        v >>= 7;
+        n += 1;
+    }
+    n
+}
+
+fn overhead(v: usize) -> usize {
+    // overhead in a repeated field is per-item, so:
+    //
+    // [tag-byte] [varint-length] [kv-bytes…]
+    varint_len(v) + 1 + v
+}
+
 impl<'a> crate::SizedGenerator<'a> for TagGenerator {
     type Output = Vec<KeyValue>;
     type Error = GeneratorError;
@@ -51,6 +72,8 @@ impl<'a> crate::SizedGenerator<'a> for TagGenerator {
     where
         R: rand::Rng + ?Sized,
     {
+        let mut inner_budget = *budget;
+
         // NOTE that the rng must be passed to the inner generator but is not
         // used by that generator: the generator maintains its own rng. It's a
         // quirk of the trait.
@@ -64,6 +87,10 @@ impl<'a> crate::SizedGenerator<'a> for TagGenerator {
         let mut attributes = Vec::<KeyValue>::with_capacity(tagset.len());
 
         for tag in tagset {
+            if inner_budget < SMALLEST_KV_PROTOBUF {
+                break;
+            }
+
             let key = self
                 .inner
                 .using_handle(tag.key)
@@ -80,19 +107,47 @@ impl<'a> crate::SizedGenerator<'a> for TagGenerator {
                 }),
             };
 
-            // NOTE when this is encoded by protobuf the Vec will have a
-            // variable length associated with it which we DO NOT consider here.
-            let required_bytes = kv.encoded_len();
-            let diff = budget.saturating_sub(required_bytes);
+            let required_bytes = overhead(kv.encoded_len());
 
-            if diff > 0 {
-                attributes.push(kv);
-                *budget = diff;
-            } else if attributes.is_empty() {
-                return Err(Self::Error::SizeExhausted);
+            match inner_budget.cmp(&required_bytes) {
+                cmp::Ordering::Equal | cmp::Ordering::Greater => {
+                    attributes.push(kv);
+                    inner_budget -= required_bytes;
+                }
+                cmp::Ordering::Less => {
+                    if attributes.is_empty() {
+                        return Err(Self::Error::SizeExhausted);
+                    }
+                    break;
+                }
             }
         }
 
+        *budget = inner_budget;
         Ok(attributes)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::{SMALLEST_KV_PROTOBUF, overhead};
+    use opentelemetry_proto::tonic::common::v1::{AnyValue, KeyValue, any_value};
+    use prost::Message;
+
+    #[test]
+    fn smallest_kv_protobuf() {
+        let kv = KeyValue {
+            key: String::from("k"),
+            value: Some(AnyValue {
+                value: Some(any_value::Value::StringValue(String::from("v"))),
+            }),
+        };
+
+        let encoded_size = overhead(kv.encoded_len());
+
+        assert!(
+            encoded_size == SMALLEST_KV_PROTOBUF,
+            "Minimal useful request size ({encoded_size}) should be == SMALLEST_KV_PROTOBUF ({SMALLEST_KV_PROTOBUF})"
+        );
     }
 }
