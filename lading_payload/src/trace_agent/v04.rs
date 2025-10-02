47c22e69a0fcb51de063d8e0ccf731d64797d2c2
Brian L. Troutwine <brian.troutwine@datadoghq.com>
2025-10-02 14:00:33 -0700

    serialize as struct map?
    Signed-off-by: Brian L. Troutwine <brian.troutwine@datadoghq.com>

--- lading_payload/src/trace_agent/v04.rs
+++ lading_payload/src/trace_agent/v04.rs
@@ -490,7 +490,7 @@ impl crate::Serialize for V04 {
         }
         loop {
             let mut buf = Vec::with_capacity(max_bytes);
-            traces.serialize(&mut Serializer::new(&mut buf))?;
+            traces.serialize(&mut Serializer::new(&mut buf).with_struct_map())?;
 
             if buf.len() > max_bytes {
                 break;
@@ -510,7 +510,7 @@ impl crate::Serialize for V04 {
         while low < high {
             let mid = (low + high).div_ceil(2);
             let mut buf = Vec::with_capacity(max_bytes);
-            traces[0..mid].serialize(&mut Serializer::new(&mut buf))?;
+            traces[0..mid].serialize(&mut Serializer::new(&mut buf).with_struct_map())?;
 
             if buf.len() <= max_bytes {
                 low = mid;
@@ -520,7 +520,7 @@ impl crate::Serialize for V04 {
         }
 
         let mut buf = Vec::with_capacity(max_bytes);
-        traces[0..low].serialize(&mut Serializer::new(&mut buf))?;
+        traces[0..low].serialize(&mut Serializer::new(&mut buf).with_struct_map())?;
         writer.write_all(&buf)?;
 
         Ok(())
@@ -641,7 +641,7 @@ mod test {
 
         /// Property: Context are bounded.
         #[test]
-        fn contexts_are_bounded(seed: u64, contexts in 1u32..super::MAX_CONTEXTS, total_traces in 1u32..10_000) {
+        fn contexts_are_bounded(seed: u64, contexts in 1u32..100, total_traces in 1u32..1_000) {
             let mut rng = SmallRng::seed_from_u64(seed);
 
             let mut config = Config::default();
@@ -722,7 +722,7 @@ mod test {
         let mut serialized = Vec::new();
         let traces = vec![trace.spans];
         traces
-            .serialize(&mut rmp_serde::Serializer::new(&mut serialized))
+            .serialize(&mut rmp_serde::Serializer::new(&mut serialized).with_struct_map())
             .unwrap();
         assert_eq!(serialized.len(), 18_222);
     }
