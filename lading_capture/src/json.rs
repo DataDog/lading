e5170840d3216c6cdb22aa012c27c7f2445aa5d5
Brian L. Troutwine <brian.troutwine@datadoghq.com>
2025-10-09 16:21:12 -0700

    Add `recorded_at` to Capture JSON line
    This new field records the time in milliseconds that the value was
    recorded, with reference to the Unix Epoch. The goal is to confirm
    up-stack that historical writes are recorded at a time different from
    their `time`.
    
    Signed-off-by: Brian L. Troutwine <brian.troutwine@datadoghq.com>

--- lading_capture/src/json.rs
+++ lading_capture/src/json.rs
@@ -14,7 +14,7 @@ pub enum MetricKind {
     Gauge,
 }
 
-#[derive(Debug, Serialize, Deserialize, Clone, Copy)]
+#[derive(Debug, Serialize, Deserialize, Clone, Copy, PartialEq)]
 /// The value for [`Line`].
 #[serde(untagged)]
 pub enum LineValue {
@@ -50,6 +50,9 @@ impl std::fmt::Display for LineValue {
 pub struct Line {
     /// The time in milliseconds that this line was written.
     pub time: u128,
+    /// The time in milliseconds that this metric was recorded internally.
+    /// This may differ from `time` when historical writes are used.
+    pub recorded_at: u128,
     /// The "fetch index". Previous versions of lading scraped prometheus
     /// metrics from their targets and kept an increment index of polls. Now
     /// this records the number of times the internal metrics cache has been
