e09373e16745fe188b6e8ddb3bc448e8513ea8e9
Jake Saferstein <jake.saferstein@datadoghq.com>
2025-10-23 14:53:14 -0400

    initial cursor pr

--- lading/build.rs
+++ lading/build.rs
@@ -4,10 +4,16 @@ fn main() -> std::io::Result<()> {
     println!("cargo:rerun-if-changed=proto/");
 
     let includes = ["proto/"];
+
     prost_build::Config::new()
         .out_dir("src/proto/")
         .protoc_arg("--experimental_allow_proto3_optional")
         .compile_protos(&["proto/agent_payload.proto"], &includes)?;
+
+    // Compile stateful_encoding.proto with gRPC services
+    tonic_build::configure()
+        .out_dir("src/proto/")
+        .compile_protos(&["proto/stateful_encoding.proto"], &includes)?;
 
     Ok(())
 }
