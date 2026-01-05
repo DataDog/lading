891a60471cb8d788b57dffaa03d836435286d8c3
Jake Saferstein <jake.saferstein@datadoghq.com>
2026-01-05 10:54:39 -0500

    throttler error cleanup

--- lading/src/generator/tcp.rs
+++ lading/src/generator/tcp.rs
@@ -272,7 +272,7 @@ impl TcpWorker {
                             }
                         }
                         Err(err) => {
-                            error!("Throttle request of {total_bytes} is larger than throttle capacity. Block will be discarded. Error: {err}");
+                            error!("Discarding block due to throttle error: {err}");
                         }
                     }
                 }
