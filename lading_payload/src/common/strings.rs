ea5115823c91999da65d9706a882444a3568a00d
Stephen Wakely <fungus.humungus@gmail.com>
2026-01-29 16:37:26 +0000

    Add proptest for string list pool

--- lading_payload/src/common/strings.rs
+++ lading_payload/src/common/strings.rs
@@ -51,7 +51,7 @@ pub(crate) trait Pool {
 }
 
 #[enum_dispatch(Pool)]
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 pub(crate) enum PoolKind {
     RandomStringPool(RandomStringPool),
     StringListPool(StringListPool),
@@ -75,61 +75,4 @@ where
     F: FnOnce(&mut R) -> Option<T>,
 {
     if rng.random() { func(rng) } else { None }
-}
-
-#[cfg(test)]
-mod test {
-    use proptest::prelude::*;
-
-    use super::RandomStringPool;
-    use super::random_string_pool::ALPHANUM;
-    use rand::{SeedableRng, rngs::SmallRng};
-
-    // Ensure that no returned string ever has a non-alphabet character.
-    proptest! {
-        #[test]
-        fn no_nonalphabet_char(seed: u64, max_bytes: u16, of_size_bytes: u16) {
-            let max_bytes = max_bytes as usize;
-            let of_size_bytes = of_size_bytes as usize;
-            let mut rng = SmallRng::seed_from_u64(seed);
-
-            let pool = RandomStringPool::with_size_and_alphabet(&mut rng, max_bytes, ALPHANUM);
-            if let Some(s) = pool.of_size(&mut rng, of_size_bytes) {
-                for c in s.bytes() {
-                    assert!(ALPHANUM.contains(&c));
-                }
-            }
-        }
-    }
-
-    // Ensure that no returned string is ever larger or smaller than of_size_bytes.
-    proptest! {
-        #[test]
-        fn no_size_mismatch(seed: u64, max_bytes: u16, of_size_bytes: u16) {
-            let max_bytes = max_bytes as usize;
-            let of_size_bytes = of_size_bytes as usize;
-            let mut rng = SmallRng::seed_from_u64(seed);
-
-            let pool = RandomStringPool::with_size_and_alphabet(&mut rng, max_bytes, ALPHANUM);
-            if let Some(s) = pool.of_size(&mut rng, of_size_bytes) {
-                assert!(s.len() == of_size_bytes);
-            }
-        }
-    }
-
-    // Ensure that of_size only returns None if the request is greater than or
-    // equal to the interior size.
-    proptest! {
-        #[test]
-        fn return_none_condition(seed: u64, max_bytes: u16, of_size_bytes: u16) {
-            let max_bytes = max_bytes as usize;
-            let of_size_bytes = of_size_bytes as usize;
-            let mut rng = SmallRng::seed_from_u64(seed);
-
-            let pool = RandomStringPool::with_size_and_alphabet(&mut rng, max_bytes, ALPHANUM);
-            if pool.of_size(&mut rng, of_size_bytes).is_none() {
-                assert!(of_size_bytes >= max_bytes);
-            }
-        }
-    }
 }
--- lading_payload/src/common/strings/string_list_pool.rs
+++ lading_payload/src/common/strings/string_list_pool.rs
@@ -1,12 +1,22 @@
 use super::{Handle, Pool};
 
-#[derive(Debug, Clone)]
+/// A list of strings
+///
+/// It can be useful to configure lading to create strings from a fixed list
+/// to control the shape of data emitted. The pool will hand out a random string
+/// from the list.
+#[derive(Debug)]
 pub(crate) struct StringListPool {
     metric_names: Vec<String>,
 }
 
 impl StringListPool {
+    /// Create a new list of strings.
     pub(crate) fn new(metric_names: Vec<String>) -> Self {
+        assert!(
+            !metric_names.is_empty(),
+            "don't create a string list with an empty list"
+        );
         Self { metric_names }
     }
 }
@@ -25,5 +35,36 @@ impl Pool for StringListPool {
             .as_index()
             .expect("handle for string list pool should be a string pool handle");
         self.metric_names.get(idx).map(|s| s.as_str())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use proptest::prelude::*;
+
+    use super::{Pool, StringListPool};
+    use rand::{SeedableRng, rngs::SmallRng};
+
+    // Ensure that no returned string ever has a non-alphabet character.
+    proptest! {
+        #[test]
+        fn string_is_in_list(seed: u64, mut names: Vec<String> ) {
+            // Make sure the list is never empty.
+            names.push("thing".to_string());
+
+            let mut rng = SmallRng::seed_from_u64(seed);
+
+            let pool = StringListPool::new(names.clone());
+            if let Some((s1, h)) = pool.of_size_with_handle(&mut rng, 0) {
+                if let Some(s2) = pool.using_handle(h) {
+                    assert_eq!(s1, s2);
+                    assert!(names.iter().any(|s| s == s2));
+                } else {
+                    panic!("could not get string with handle");
+                }
+            } else {
+                panic!("could not get handle from pool");
+            }
+        }
     }
 }
