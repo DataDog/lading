DataDog/lading

commit 2cbee5eb901555f3751922c4192a89c7b427d8b2

[AMCC-168]Allow configuring metric names, tag names and values (#1732)
* Allow metric names to be configurable.

* Make tags everywhere

* Use generics with associated type everywhere

* Use enum dispatch.

* Use constraints in trait.

* Configure tag values

* Add proptest for string list pool

* Add tagset proptests

* Clippy

* Updated fingerprint.

* Added to changelog

lading_payload/src/dogstatsd/event.rs
@@ -1,20 +1,20 @@
 //! `DogStatsD` event.
-use std::{fmt, ops::Range, rc::Rc};
+use std::{fmt, ops::Range};
 
 use rand::{Rng, distr::StandardUniform, prelude::Distribution};
 
 use crate::{Error, Generator, common::strings};
 
-use self::strings::choose_or_not_fn;
+use self::strings::{Pool, choose_or_not_fn};
 
-use super::{ConfRange, common};
+use super::{ConfRange, StringPools, common};
 
 #[derive(Debug, Clone)]
 pub(crate) struct EventGenerator {
     pub(crate) title_length: ConfRange<u16>,
     pub(crate) texts_or_messages_length_range: Range<u16>,
     pub(crate) small_strings_length_range: Range<u16>,
-    pub(crate) str_pool: Rc<strings::Pool>,
+    pub(crate) pools: StringPools,
     pub(crate) tags_generator: common::tags::Generator,
 }
 
@@ -28,11 +28,13 @@ impl<'a> Generator<'a> for EventGenerator {
     {
         let title_sz = self.title_length.sample(&mut rng) as usize;
         let title = self
-            .str_pool
+            .pools
+            .randomstring
             .of_size(&mut rng, title_sz)
             .ok_or(Error::StringGenerate)?;
         let text = self
-            .str_pool
+            .pools
+            .randomstring
             .of_size_range(&mut rng, self.texts_or_messages_length_range.clone())
             .ok_or(Error::StringGenerate)?;
         let tags = if rng.random() {
@@ -48,21 +50,24 @@ impl<'a> Generator<'a> for EventGenerator {
             text,
             timestamp_second: rng.random_bool(0.5).then(|| rng.random()),
             hostname: choose_or_not_fn(&mut rng, |r| {
-                self.str_pool
+                self.pools
+                    .randomstring
                     .of_size_range(r, self.small_strings_length_range.clone())
             }),
             aggregation_key: choose_or_not_fn(&mut rng, |r| {
-                self.str_pool
+                self.pools
+                    .randomstring
                     .of_size_range(r, self.small_strings_length_range.clone())
             }),
             priority: rng.random_bool(0.5).then(|| rng.random()),
             source_type_name: choose_or_not_fn(&mut rng, |r| {
-                self.str_pool
+                self.pools
+                    .randomstring
                     .of_size_range(r, self.small_strings_length_range.clone())
             }),
             alert_type: rng.random_bool(0.5).then(|| rng.random()),
             tags,
-            str_pool: &self.str_pool,
+            pools: &self.pools,
         })
     }
 }
@@ -93,7 +98,7 @@ pub struct Event<'a> {
     /// Tags of the event
     pub(crate) tags: Option<common::tags::Tagset>,
     /// String pool for tag handle lookups during serialization.
-    pub(crate) str_pool: &'a strings::Pool,
+    pub(crate) pools: &'a StringPools,
 }
 
 impl fmt::Display for Event<'_> {
@@ -132,11 +137,13 @@ impl fmt::Display for Event<'_> {
             let mut commas_remaining = tags.len() - 1;
             for tag in tags {
                 let key = self
-                    .str_pool
+                    .pools
+                    .tag_name
                     .using_handle(tag.key)
                     .expect("invalid tag key handle");
                 let value = self
-                    .str_pool
+                    .pools
+                    .randomstring
                     .using_handle(tag.value)
                     .expect("invalid tag value handle");
                 write!(f, "{key}:{value}")?;

lading_payload/src/dogstatsd/service_check.rs
@@ -1,14 +1,17 @@
 //! `DogStatsD` service check.
-use std::{fmt, rc::Rc};
+use std::fmt;
 
 use rand::{Rng, distr::StandardUniform, prelude::Distribution, seq::IndexedRandom};
 
 use crate::{
     Error, Generator,
-    common::strings::{self, choose_or_not_ref},
+    common::strings::{Pool, choose_or_not_ref},
 };
 
-use super::common::{self, tags::Tagset};
+use super::{
+    StringPools,
+    common::{self, tags::Tagset},
+};
 
 #[derive(Debug, Clone)]
 pub(crate) struct ServiceCheckGenerator {
@@ -16,7 +19,7 @@ pub(crate) struct ServiceCheckGenerator {
     pub(crate) small_strings: Vec<String>,
     pub(crate) texts_or_messages: Vec<String>,
     pub(crate) tags_generator: common::tags::Generator,
-    pub(crate) str_pool: Rc<strings::Pool>,
+    pub(crate) pools: StringPools,
 }
 
 impl<'a> Generator<'a> for ServiceCheckGenerator {
@@ -43,7 +46,7 @@ impl<'a> Generator<'a> for ServiceCheckGenerator {
             hostname,
             tags,
             message,
-            str_pool: &self.str_pool,
+            pools: &self.pools,
         })
     }
 }
@@ -64,7 +67,7 @@ pub struct ServiceCheck<'a> {
     /// Message of the service check.
     pub message: Option<&'a str>,
     /// String pool for tag handle lookups during serialization.
-    pub(crate) str_pool: &'a strings::Pool,
+    pub(crate) pools: &'a StringPools,
 }
 
 impl fmt::Display for ServiceCheck<'_> {
@@ -89,11 +92,13 @@ impl fmt::Display for ServiceCheck<'_> {
             let mut commas_remaining = tags.len() - 1;
             for tag in tags {
                 let key = self
-                    .str_pool
+                    .pools
+                    .randomstring
                     .using_handle(tag.key)
                     .expect("invalid tag key handle");
                 let value = self
-                    .str_pool
+                    .pools
+                    .tag_name
                     .using_handle(tag.value)
                     .expect("invalid tag value handle");
                 write!(f, "{key}:{value}")?;

Cargo.toml
@@ -67,6 +67,7 @@ ddsketch-agent = { git = "https://github.com/DataDog/saluki", rev = "f47a7ef588c
 ] }
 datadog-protos = { git = "https://github.com/DataDog/saluki", rev = "f47a7ef588c53aa1da35dcfd93808595ebeb1291" }
 protobuf = { version = "3.7" }
+enum_dispatch = { version = "0.3" }
 
 [workspace.lints.clippy]
 all = "deny"

lading/src/generator.rs
@@ -204,7 +204,7 @@ impl Server {
             Inner::FileTree(conf) => Self::FileTree(file_tree::FileTree::new(&conf, shutdown)?),
             Inner::Grpc(conf) => Self::Grpc(grpc::Grpc::new(config.general, conf, shutdown)?),
             Inner::UnixStream(conf) => {
-                if let lading_payload::Config::DogStatsD(variant) = conf.variant
+                if let lading_payload::Config::DogStatsD(variant) = conf.variant.clone()
                     && !variant.length_prefix_framed
                 {
                     warn!(

lading_payload/Cargo.toml
@@ -27,6 +27,7 @@ time = { version = "0.3", features = ["formatting"] }
 tracing = { workspace = true }
 tokio = { workspace = true }
 arbitrary = { version = "1", optional = true, features = ["derive"] }
+enum_dispatch = { workspace = true }
 
 [dev-dependencies]
 proptest = { workspace = true }

lading_payload/src/apache_common.rs
@@ -301,7 +301,7 @@ impl fmt::Display for Member<'_> {
 #[derive(Debug, Clone)]
 /// Apache Common log format payload
 pub struct ApacheCommon {
-    str_pool: strings::Pool,
+    str_pool: strings::RandomStringPool,
 }
 
 impl ApacheCommon {
@@ -311,7 +311,7 @@ impl ApacheCommon {
         R: rand::Rng + ?Sized,
     {
         Self {
-            str_pool: strings::Pool::with_size(rng, 1_000_000),
+            str_pool: strings::RandomStringPool::with_size(rng, 1_000_000),
         }
     }
 }

lading_payload/src/ascii.rs
@@ -11,7 +11,7 @@ const MAX_LENGTH: u16 = 6_144; // 6 KiB
 #[derive(Debug, Clone)]
 /// ASCII text payload
 pub struct Ascii {
-    pool: strings::Pool,
+    pool: strings::RandomStringPool,
 }
 
 impl Ascii {
@@ -23,7 +23,7 @@ impl Ascii {
         Self {
             // SAFETY: Do not adjust this downward below MAX_LENGTH without also
             // adjusting the input to `self.pool.of_size` below.
-            pool: strings::Pool::with_size(rng, usize::from(MAX_LENGTH * 4)),
+            pool: strings::RandomStringPool::with_size(rng, usize::from(MAX_LENGTH * 4)),
         }
     }
 }

lading_payload/src/block.rs
@@ -254,7 +254,7 @@ impl Cache {
                         return Err(Error::InvalidConfig(e));
                     }
                 }
-                let mut serializer = crate::DogStatsD::new(*conf, &mut rng)?;
+                let mut serializer = crate::DogStatsD::new(conf.clone(), &mut rng)?;
 
                 let span = span!(Level::INFO, "fixed", payload = "dogstatsd");
                 let _guard = span.enter();

commit 1b11acfe1941afa5b1fc19c5fa3b7d849af7669c

Fix incorrect pool assignment in dogstatsd/event, service_check
This commit corrects a small bug introduced in PR #1732. I have a
notion of how to make these pool handles type-safe but that's an
intrusive change I do not want to bundle here.

There is a stray deny.toml change present in this PR which will
disappear once PR #1749 hits main.

lading_payload/src/dogstatsd/event.rs
@@ -143,7 +143,7 @@ impl fmt::Display for Event<'_> {
                     .expect("invalid tag key handle");
                 let value = self
                     .pools
-                    .randomstring
+                    .tag_value
                     .using_handle(tag.value)
                     .expect("invalid tag value handle");
                 write!(f, "{key}:{value}")?;

lading_payload/src/dogstatsd/service_check.rs
@@ -93,12 +93,12 @@ impl fmt::Display for ServiceCheck<'_> {
             for tag in tags {
                 let key = self
                     .pools
-                    .randomstring
+                    .tag_name
                     .using_handle(tag.key)
                     .expect("invalid tag key handle");
                 let value = self
                     .pools
-                    .tag_name
+                    .tag_value
                     .using_handle(tag.value)
                     .expect("invalid tag value handle");
                 write!(f, "{key}:{value}")?;